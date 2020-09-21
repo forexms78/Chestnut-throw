aae6978633015040e9841afadbcf299e327be576
forexms78 <49295076+forexms78@users.noreply.github.com>
2020-09-21 17:59:56 +0900

    Add files via upload

---
+++ BamsongiController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BamsongiController : MonoBehaviour
+{
+
+    public float lifetime = 7.0f;
+
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    private void Start()
+    {
+        //shoot(new Vector3(0, 200, 2000));
+    }
+
+    void Awake()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
